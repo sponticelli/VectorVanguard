48ce66f0de4961d6a2fb949acb6d7aac2e0d33fb
sponticelli <sandro.ponticelli@gmail.com>
2023-03-23 09:11:38 +0100

    feat: add collider generation to VectorMeshRendererEditor

--- Assets/Scripts/Vektor/Editor/MeshFilters/VectorConnectedMeshFilterEditor.cs
+++ Assets/Scripts/Vektor/Editor/MeshFilters/VectorConnectedMeshFilterEditor.cs
@@ -23,8 +23,6 @@ namespace Vektor.Editors
       
     }
     
-
-
     private void CacheData()
     {
       _transform = ((VectorConnectedMeshFilter)target).transform;
@@ -38,11 +36,15 @@ namespace Vektor.Editors
     }
 
     
-    
     public override void OnInspectorGUI()
     {
       base.OnInspectorGUI();
       EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+      GenerateButton();
+    }
+
+    private void GenerateButton()
+    {
       _scaleFactor = EditorGUILayout.FloatField("Scale Factor", _scaleFactor);
       if (GUILayout.Button("Generate"))
       {
@@ -53,14 +55,15 @@ namespace Vektor.Editors
           {
             points[i] *= _scaleFactor;
           }
+
           ((VectorConnectedMeshFilter)target).Points = points;
         }
-        
-        ((VectorConnectedMeshFilter) target).Generate(true);
+
+        ((VectorConnectedMeshFilter)target).Generate(true);
         CacheData();
       }
     }
-    
+
     private void OnSceneGUI()
     {
       var numPoints = ((VectorConnectedMeshFilter)target).Points.Length;
@@ -73,54 +76,64 @@ namespace Vektor.Editors
       
       var meshFilter = (VectorConnectedMeshFilter) target;
       
-      
       // Draw handles for each point
       for (var i = 0; i < _worldPoints.Count; i++)
       {
-        var worldPoint = _worldPoints[i];
-        EditorGUI.BeginChangeCheck();
-        worldPoint = Handles.PositionHandle(worldPoint, Quaternion.identity);
-        if (EditorGUI.EndChangeCheck())
-        {
-          // Convert the world point back to local space and update the _points list
-          var localPoint = _transform.InverseTransformPoint(worldPoint);
-          meshFilter.Points[i] = localPoint;
-          _worldPoints[i] = worldPoint;
-          meshFilter.Generate(true);
-        }
-        
-        // Draw lines between adjacent points
+        DrawPointHandle(i, meshFilter);
+
         var previousColor = Handles.color;
-        Handles.color = Color.blue;
-        var j = (i + 1) % _worldPoints.Count;
-        Handles.DrawLine(_worldPoints[i], _worldPoints[j], 1);
-        
+        DrawLineBetweenPoints(i, out var j);
         // Add a "+" button in the middle of the line
-        var midpoint = (_worldPoints[i] + _worldPoints[j]) * 0.5f;
-        Handles.color = Color.green;
-        if (Handles.Button(midpoint, Quaternion.identity, HandleUtility.GetHandleSize(midpoint) * 0.1f, 
-              HandleUtility.GetHandleSize(midpoint) * 0.1f, Handles.CircleHandleCap ))
-        {
-          // Insert a new point in the middle
-          var points = meshFilter.Points;
-          meshFilter.Points = new Vector3[points.Length + 1];
-          for (var k = 0; k < i + 1; k++)
-          {
-            meshFilter.Points[k] = points[k];
-          }
-          meshFilter.Points[i + 1] = _transform.InverseTransformPoint(midpoint);
-          for (var k = i + 2; k < meshFilter.Points.Length; k++)
-          {
-            meshFilter.Points[k] = points[k - 1];
-          }
-          
-          CacheData();
-          meshFilter.Generate(true);
-        }
-        
-        
+        AddMidPoint(i, j, meshFilter);
         Handles.color = previousColor;
       }
+    }
+
+    private void DrawPointHandle(int i, VectorConnectedMeshFilter meshFilter)
+    {
+      var worldPoint = _worldPoints[i];
+      EditorGUI.BeginChangeCheck();
+      worldPoint = Handles.PositionHandle(worldPoint, Quaternion.identity);
+      if (EditorGUI.EndChangeCheck())
+      {
+        // Convert the world point back to local space and update the _points list
+        var localPoint = _transform.InverseTransformPoint(worldPoint);
+        meshFilter.Points[i] = localPoint;
+        _worldPoints[i] = worldPoint;
+        meshFilter.Generate(true);
+      }
+    }
+
+    private void DrawLineBetweenPoints(int i, out int j)
+    {
+      // Draw lines between adjacent points
+      Handles.color = Color.blue;
+      j = (i + 1) % _worldPoints.Count;
+      Handles.DrawLine(_worldPoints[i], _worldPoints[j], 1);
+    }
+
+    private void AddMidPoint(int i, int j, VectorConnectedMeshFilter meshFilter)
+    {
+      var midpoint = (_worldPoints[i] + _worldPoints[j]) * 0.5f;
+      Handles.color = Color.green;
+      if (!Handles.Button(midpoint, Quaternion.identity, HandleUtility.GetHandleSize(midpoint) * 0.1f,
+            HandleUtility.GetHandleSize(midpoint) * 0.1f, Handles.CircleHandleCap)) return;
+      // Insert a new point in the middle
+      var points = meshFilter.Points;
+      meshFilter.Points = new Vector3[points.Length + 1];
+      for (var k = 0; k < i + 1; k++)
+      {
+        meshFilter.Points[k] = points[k];
+      }
+
+      meshFilter.Points[i + 1] = _transform.InverseTransformPoint(midpoint);
+      for (var k = i + 2; k < meshFilter.Points.Length; k++)
+      {
+        meshFilter.Points[k] = points[k - 1];
+      }
+
+      CacheData();
+      meshFilter.Generate(true);
     }
   }
 }
