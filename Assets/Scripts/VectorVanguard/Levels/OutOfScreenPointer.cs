1ca12f7dfa41ee60b4efc46e3226c4841eebf5e5
sponticelli <sandro.ponticelli@gmail.com>
2023-04-14 10:51:52 +0200

    feat: OutOfScreenPointer behaviour completed

--- Assets/Scripts/VectorVanguard/Levels/OutOfScreenPointer.cs
+++ Assets/Scripts/VectorVanguard/Levels/OutOfScreenPointer.cs
@@ -14,6 +14,8 @@ namespace VectorVanguard.Levels
     [SerializeField] private Actor _target;
     [SerializeField] private float _distanceFromBorder = 0.1f;
     [SerializeField] private Actor _player;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private GameObject _pointer;
 
     public Actor Actor
     {
@@ -22,30 +24,65 @@ namespace VectorVanguard.Levels
     }
 
     private Vector3 _screenCenter = Vector3.zero;
-    private Vector3 _horiziontalIntersectionPoint = Vector3.zero;
-    private Vector3 _verticalIntersectionPoint = Vector3.zero;
+    
+    private Vector3 _intersectionPoint = Vector3.zero;
     private Vector3 _direction = Vector3.zero;
-
-    private bool _drawVertical = false;
-    private bool _drawHorizontal = false;
+    
+    private bool _draw = false;
     
     
-    public void Initialization(Actor actor, Actor player)
+    public void Initialization(Actor actor, Actor player, Camera camera)
     {
       _player = player;
       _target = actor;
       _screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+      _pointer.SetActive(false);
+      _camera = camera;
+    }
+
+    private void Awake()
+    {
+      if (_camera == null)
+      {
+        _camera = Camera.main;
+      }
     }
 
 
     private void Update()
     {
-      if (_target == null)
+      if (CalcPosition())
       {
-        gameObject.SetActive(false);
-        return;
+        EnablePointer();
       }
+      else
+      {
+        DisablePointer();
+      }
+    }
+    
+    private void DisablePointer()
+    {
+      _pointer.SetActive(false);
+    }
 
+    private void EnablePointer()
+    {
+      _pointer.SetActive(true);
+      _intersectionPoint = _camera.ScreenToWorldPoint(_intersectionPoint) + _direction * _distanceFromBorder;
+      _intersectionPoint.z = 0;
+      transform.position = _intersectionPoint;
+    }
+
+    private bool CalcPosition()
+    {
+      var targetScreenPosition = _camera.WorldToScreenPoint(_target.transform.position);
+      if (targetScreenPosition.x > 0 && targetScreenPosition.x < Screen.width &&
+          targetScreenPosition.y > 0 && targetScreenPosition.y < Screen.height)
+      {
+        return false;
+      }
+      
       //Camera center in world space with z = 0
       _direction = _target.transform.position - _player.transform.position;
       _direction.z = 0;
@@ -53,55 +90,40 @@ namespace VectorVanguard.Levels
       //find the angle of the direction
       var angle = -90 + Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
       transform.rotation = Quaternion.Euler(0, 0, angle);
-      
+
       // Convert the player position to screen space
-      var targetScreenPosition = Camera.main.WorldToScreenPoint(_target.transform.position);
-      var playerScreenPosition = Camera.main.WorldToScreenPoint(_player.transform.position);
+      var playerScreenPosition = _camera.WorldToScreenPoint(_player.transform.position);
       // Find the intersection of a line from target screen position with a screen border with the given direction _direction
-      _horiziontalIntersectionPoint = Vector3.zero;
-      _drawHorizontal = false;
+      _draw = false;
       if (targetScreenPosition.x < playerScreenPosition.x)
       {
-        _horiziontalIntersectionPoint = FindIntersection(playerScreenPosition, _direction, Side.Left);
+        _draw = FindIntersection(playerScreenPosition, _direction, Side.Left, out _intersectionPoint);
       }
       else if (targetScreenPosition.x > playerScreenPosition.x)
       {
-        _horiziontalIntersectionPoint = FindIntersection(playerScreenPosition, _direction, Side.Right);
+        _draw = FindIntersection(playerScreenPosition, _direction, Side.Right, out _intersectionPoint);
       }
+
+      if (_draw) return _draw;
       
-      _verticalIntersectionPoint = Vector3.zero;
-      _drawVertical = false;
       if (targetScreenPosition.y < playerScreenPosition.y)
       {
-        _verticalIntersectionPoint = FindIntersection(playerScreenPosition, _direction, Side.Bottom);
+        _draw =
+          FindIntersection(playerScreenPosition, _direction, Side.Bottom, out _intersectionPoint);
       }
       else if (targetScreenPosition.y > playerScreenPosition.y)
       {
-        _verticalIntersectionPoint = FindIntersection(playerScreenPosition, _direction, Side.Top);
+        _draw = FindIntersection(playerScreenPosition, _direction, Side.Top, out _intersectionPoint);
       }
 
-
-      // Convert the intersection point to world space
-      if (_verticalIntersectionPoint != Vector3.zero)
-      {
-        _verticalIntersectionPoint = Camera.main.ScreenToWorldPoint(_verticalIntersectionPoint);
-        _verticalIntersectionPoint.z = 0;
-        _drawVertical = true;
-      }
-
-      if (_verticalIntersectionPoint != Vector3.zero)
-      {
-        _horiziontalIntersectionPoint = Camera.main.ScreenToWorldPoint(_horiziontalIntersectionPoint);
-        _horiziontalIntersectionPoint.z = 0;
-        _drawHorizontal = true;
-      }
+      return _draw;
     }
 
     private void OnDrawGizmos()
     {
+      if (!_draw) return;
       GizmoHelper.Arrow(transform.position, _direction, Color.red);
-      if (_drawVertical) GizmoHelper.Point(_verticalIntersectionPoint, 1f, Color.yellow);
-      if (_drawHorizontal) GizmoHelper.Point(_horiziontalIntersectionPoint, 1f, Color.yellow);
+      GizmoHelper.Point(_intersectionPoint, 1f, Color.yellow);
     }
 
   
@@ -113,14 +135,14 @@ namespace VectorVanguard.Levels
       Right
     }
     
-    private Vector3 FindIntersection(Vector3 screenPosition, Vector3 direction, Side side)
+    private bool FindIntersection(Vector3 screenPosition, Vector3 direction, Side side, out Vector3 intersectionPoint)
     {
-      var intersectionPoint = Vector3.zero;
+      intersectionPoint = Vector3.zero;
       var screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
       
       //four corners of the screen
-      Vector3 borderPoint = Vector3.zero;
-      Vector3 borderDirection = Vector3.zero;
+      var borderPoint = Vector3.zero;
+      var borderDirection = Vector3.zero;
       switch (side)
       {
         case Side.Top:
@@ -155,11 +177,11 @@ namespace VectorVanguard.Levels
         if (intersectionPoint.x >= 0 && intersectionPoint.x <= Screen.width && 
             intersectionPoint.y >= 0 && intersectionPoint.y <= Screen.height)
         {
-          return intersectionPoint;
+          return true;
         }
       }
       
-      return intersectionPoint;
+      return false;
     }
 
     public Vector2 GetIntersectionPointCoordinates(Vector2 A1, Vector2 A2, Vector2 B1, Vector2 B2, out bool found)
