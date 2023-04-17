6dde4bcb6f9351cc112326b7a7efbaa196c0d85b
sponticelli <sandro.ponticelli@gmail.com>
2023-04-17 09:25:33 +0200

    feat: cache curve length

--- Assets/Scripts/VectorVanguard/Utils/Paths/CubicBezierCurve.cs
+++ Assets/Scripts/VectorVanguard/Utils/Paths/CubicBezierCurve.cs
@@ -4,35 +4,29 @@ using UnityEngine;
 namespace VectorVanguard.Utils.Paths
 {
   [Serializable]
-  public class CubicBezierCurve : ICurve
+  public class CubicBezierCurve : ACurve
   {
-    [SerializeField] private Vector3 _startPoint;
     [SerializeField] private Vector3 _controlPoint1;
     [SerializeField] private Vector3 _controlPoint2;
-    [SerializeField] private Vector3 _endPoint;
-
-    public Vector3 StartPoint
-    {
-      get => _startPoint;
-      set => _startPoint = value;
-    }
 
     public Vector3 ControlPoint1
     {
       get => _controlPoint1;
-      set => _controlPoint1 = value;
+      set
+      {
+        _controlPoint1 = value;
+        _length = -1;
+      }
     }
 
     public Vector3 ControlPoint2
     {
       get => _controlPoint2;
-      set => _controlPoint2 = value;
-    }
-
-    public Vector3 EndPoint
-    {
-      get => _endPoint;
-      set => _endPoint = value;
+      set
+      {
+        _controlPoint2 = value;
+        _length = -1;
+      }
     }
 
     public CubicBezierCurve(Vector3 startPoint, Vector3 controlPoint1, Vector3 controlPoint2, Vector3 endPoint)
@@ -43,27 +37,12 @@ namespace VectorVanguard.Utils.Paths
       _endPoint = endPoint;
     }
 
-    public Vector3 GetPoint(float t)
+    public override Vector3 GetPoint(float t)
     {
       return (1 - t) * (1 - t) * (1 - t) * _startPoint +
              3 * (1 - t) * (1 - t) * t * _controlPoint1 +
              3 * (1 - t) * t * t * _controlPoint2 + 
              t * t * t * _endPoint;
-    }
-
-    public float Length(int samples = 100)
-    {
-      //Calculate the length of the curve by sampling it at a given number of points
-      var length = 0f;
-      var lastPoint = GetPoint(0);
-      for (var i = 1; i <= samples; i++)
-      {
-        var t = i / (float) samples;
-        var point = GetPoint(t);
-        length += Vector3.Distance(point, lastPoint);
-        lastPoint = point;
-      }
-      return length;
     }
   }
 }
--- Assets/Scripts/VectorVanguard/Utils/Paths/QuadraticBezierCurve.cs
+++ Assets/Scripts/VectorVanguard/Utils/Paths/QuadraticBezierCurve.cs
@@ -4,50 +4,25 @@ using UnityEngine;
 namespace VectorVanguard.Utils.Paths
 {
   [Serializable]
-  public class QuadraticBezierCurve : ICurve
+  public class QuadraticBezierCurve : ACurve
   {
-    [SerializeField] private Vector3 _startPoint;
     [SerializeField] private Vector3 _controlPoint1;
-    [SerializeField] private Vector3 _endPoint;
-
-    public Vector3 StartPoint
-    {
-      get => _startPoint;
-      set => _startPoint = value;
-    }
-
     public Vector3 ControlPoint1
     {
       get => _controlPoint1;
-      set => _controlPoint1 = value;
+      set
+      {
+        _controlPoint1 = value;
+        _length = -1;
+      }
     }
 
-    public Vector3 EndPoint
-    {
-      get => _endPoint;
-      set => _endPoint = value;
-    }
-
-    public Vector3 GetPoint(float t)
+    public override Vector3 GetPoint(float t)
     {
       return (1 - t) * (1 - t) * _startPoint +
              2 * (1 - t) * t * _controlPoint1 +
              t * t * _endPoint;
     }
     
-    public float Length(int samples = 100)
-    {
-      //Calculate the length of the curve by sampling it at a given number of points
-      var length = 0f;
-      var lastPoint = GetPoint(0);
-      for (var i = 1; i <= samples; i++)
-      {
-        var t = i / (float) samples;
-        var point = GetPoint(t);
-        length += Vector3.Distance(point, lastPoint);
-        lastPoint = point;
-      }
-      return length;
-    }
   }
 }
